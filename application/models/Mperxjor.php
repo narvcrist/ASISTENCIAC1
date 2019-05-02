bd8af1cbfc03a6e58c25222fcb24d85c31122b69
narvcrist <narvcrist-97@hotmail.com>
2019-05-02 12:22:34 -0500

    Validando

--- application/models/Mperxjor.php
+++ application/models/Mperxjor.php
@@ -55,8 +55,8 @@ class Mperxjor extends CI_Model {
 
 			//validación...
 			$sqlREPETICION="select count(*) NUM_PERSONAXJORNADA 
-                from personaxjornada
-                where perxjor_sec_persona='{$PERXJOR_SEC_PERSONA}'
+                from PERSONAXJORNADA
+                where PERXJOR_SEC_PERSONA='{$PERXJOR_SEC_PERSONA}'
                 and PERXJOR_SEC_JORNADA='{$PERXJOR_SEC_JORNADA}'
                 and perxjor_estado=0";
             $NUM_PERSONAXJORNADA=$this->db->query($sqlREPETICION)->row()->NUM_PERSONAXJORNADA;
@@ -84,16 +84,40 @@ class Mperxjor extends CI_Model {
 			
 			//VARIABLES DE INGRESO
 			$PERXJOR_SEC_JORNADA=$this->input->post('jornada');
-            $PERXJOR_SEC_PERSONA=$this->input->post('persona');					
+            $PERXJOR_SEC_PERSONA=$this->input->post('persona');	
+            
+            $sqlREPETICION1="select PERXJOR_SECUENCIAL,PERXJOR_SEC_JORNADA
+							from personaxjornada
+							where PERXJOR_SECUENCIAL='{$PERXJOR_SECUENCIAL}'
+							and perxjor_estado=0";
+			$repe1 =$this->db->query($sqlREPETICION1)->row();
+			
+			$sqlREPETICION2="select PERXJOR_SECUENCIAL,PERXJOR_SEC_JORNADA 
+							from personaxjornada
+							where PERXJOR_SEC_JORNADA='{$PERXJOR_SEC_JORNADA}'
+							and perxjor_estado=0";
+			$repe2 =$this->db->query($sqlREPETICION2)->row();
+
+			$sqlREPETICION="select count(*) NUM_REPETICION
+							from personaxjornada
+							where PERXJOR_SEC_JORNADA='{$PERXJOR_SEC_JORNADA}'
+							and perxjor_estado=0";
+			$NUM_REPETICION =$this->db->query($sqlREPETICION)->row()->NUM_REPETICION;
+			
+		if(($repe1->PERXJOR_SECUENCIAL==$repe2->PERXJOR_SECUENCIAL) or ($NUM_REPETICION==0)){
 
 			
 				$sql="UPDATE PERSONAXJORNADA SET
 							PERXJOR_SEC_JORNADA='$PERXJOR_SEC_JORNADA',
 							PERXJOR_SEC_PERSONA='$PERXJOR_SEC_PERSONA'
                  WHERE PERXJOR_SECUENCIAL=$PERXJOR_SECUENCIAL";
-         $this->db->query($sql);
-		 //print_r($sql);
-         echo json_encode(array("cod"=>1,"numero"=>$PERXJOR_SECUENCIAL,"mensaje"=>"Jornada: ".$PERXJOR_SECUENCIAL.", editado con éxito"));            
+          $this->db->query($sql);
+          //print_r($sql);
+    $HOR_SECUENCIAL=$this->db->query("select max(HOR_SECUENCIAL) SECUENCIAL from HORARIO")->row()->SECUENCIAL;
+    echo json_encode(array("cod"=>$HOR_SECUENCIAL,"numero"=>$HOR_SECUENCIAL,"mensaje"=>"Horario: ".$HOR_SECUENCIAL.", editado con éxito"));    
+}else{     
+    echo json_encode(array("cod"=>1,"numero"=>1,"mensaje"=>"!!!...El Horario Ya Existe...!!!"));
+     }            
     }
 
 }
